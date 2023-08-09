16a4e599e48e560fa8532b46eae0ab2dd6bed232
Vladyslav Poltavets <vpoltavets02@gmail.com>
2023-08-09 20:46:03 +0300

    Initial commit

---
+++ src/main/java/com/fit/me/FitMeApplication.java
@@ -0,0 +1,11 @@
+package com.fit.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitMeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FitMeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/fit/me/FitMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
